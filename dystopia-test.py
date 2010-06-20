db41b358f157445385c9f67e8398149126c5be00
Simon Howard <fraggle@gmail.com>
2010-06-20 23:49:53 +0100

    Add simply test script.

---
+++ dystopia-test.py
@@ -0,0 +1,13 @@
+
+import dystopia
+
+db = dystopia.database()
+db.open("foo.db", dystopia.CREAT | dystopia.WRITER)
+db.put(1234, "hello world")
+
+print db.get(1234)
+
+db.out(1234)
+
+print db.get(1234)
+
