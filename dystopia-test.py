3f27e4c3bd613387777e7370d17ae46be313f027
Simon Howard <fraggle@gmail.com>
2010-06-20 23:53:49 +0100

    Add some more methods.

--- dystopia-test.py
+++ dystopia-test.py
@@ -5,9 +5,9 @@ db = dystopia.database()
 db.open("foo.db", dystopia.CREAT | dystopia.WRITER)
 db.put(1234, "hello world")
 
-print db.get(1234)
+print db.get(1234), len(db)
 
 db.out(1234)
 
-print db.get(1234)
+print db.get(1234), len(db)
 
